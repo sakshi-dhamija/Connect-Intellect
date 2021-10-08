cf24602e2add97b49e073ea6df3c37633febcf4c
Sohan Bandary <bandarysohan24@gmail.com>
2021-10-08 15:07:32 +0530

    added prettier config file

--- client/src/utils/fetchData.js
+++ client/src/utils/fetchData.js
@@ -1,42 +1,42 @@
-import axios from "axios";
-import { API } from "../backend";
+import axios from 'axios';
+import { API } from '../backend';
 export const getDataAPI = async (url, token) => {
-  const res = await axios.get(`${API}/${url}`, {
-    headers: { Authorization: token },
-  });
+    const res = await axios.get(`${API}/${url}`, {
+        headers: { Authorization: token },
+    });
 
-  return res;
+    return res;
 };
 
 export const postDataAPI = async (url, post, token) => {
-  const res = await axios.post(`${API}/${url}`, post, {
-    headers: { Authorization: token },
-    withCredentials: true,
-  });
+    const res = await axios.post(`${API}/${url}`, post, {
+        headers: { Authorization: token },
+        withCredentials: true,
+    });
 
-  return res;
+    return res;
 };
 
 export const putDataAPI = async (url, post, token) => {
-  const res = await axios.put(`${API}/${url}`, post, {
-    headers: { Authorization: token },
-  });
+    const res = await axios.put(`${API}/${url}`, post, {
+        headers: { Authorization: token },
+    });
 
-  return res;
+    return res;
 };
 
 export const patchDataAPI = async (url, post, token) => {
-  const res = await axios.patch(`${API}/${url}`, post, {
-    headers: { Authorization: token },
-  });
+    const res = await axios.patch(`${API}/${url}`, post, {
+        headers: { Authorization: token },
+    });
 
-  return res;
+    return res;
 };
 
 export const deleteDataAPI = async (url, token) => {
-  const res = await axios.delete(`${API}/${url}`, {
-    headers: { Authorization: token },
-  });
+    const res = await axios.delete(`${API}/${url}`, {
+        headers: { Authorization: token },
+    });
 
-  return res;
+    return res;
 };
